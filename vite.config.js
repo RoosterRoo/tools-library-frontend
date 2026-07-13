438d5320e2e4672a9506687fce377672b52a97ea
RoosterRoo <desayan9@gmail.com>
2026-07-13 12:20:03 +0530

    Vite Config updated 1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   // Load env file based on 'mode' (development or production)
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, '');
 
   return {
     plugins: [react()],
